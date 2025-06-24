72755eebe0737706a6c35a8be9a9779eadf5d3f9
etienneauert <etiauert@gmail.com>
2025-06-24 16:59:22 +0200

    cards commit

--- pokemon/src/app/pokemoncards/pokemoncards.ts
+++ pokemon/src/app/pokemoncards/pokemoncards.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './pokemoncards.css'
 })
 export class Pokemoncards {
-
+  pokemons = [
+    { name: 'Darkrai', image: '/PokemonBilder/Darkrai_Bild.png' },
+    { name: 'Deoxys', image: '/PokemonBilder/Deoxys_Bild.png' },
+    { name: 'Genesect', image: '/PokemonBilder/Genesect_Bild.png' },
+    { name: 'Infamomo', image: '/PokemonBilder/Infamomo_Bild.png' },
+    { name: 'Marshadow', image: '/PokemonBilder/Marshadow_Bild.png' },
+    { name: 'Mew', image: '/PokemonBilder/Mew_Bild.png' },
+    { name: 'Volcanion', image: '/PokemonBilder/Volcanion_Bild.png' }
+  ];
 }
