ea18544cc297b50ea388f0140d2638f4afcd88e5
Kaan-Ceri <kaan.ceri@edu.siemens-energy.com>
2025-06-25 09:59:44 +0200

    neueP

--- pokemon/src/app/pokemoncards/pokemoncards.ts
+++ pokemon/src/app/pokemoncards/pokemoncards.ts
@@ -8,12 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Pokemoncards {
   pokemons = [
-    { name: 'Darkrai', image: '/PokemonBilder/Darkrai_Bild.png' },
+    { name: 'Mew', image: '/PokemonBilder/Mew_Bild.png', Audio: '/PokemonAudio/Mew_Audio.mp3' },
+    { name: 'Celebi', image: '/PokemonBilder/Celebi_Bild.png' },
     { name: 'Deoxys', image: '/PokemonBilder/Deoxys_Bild.png' },
+    { name: 'Darkrai', image: '/PokemonBilder/Darkrai_Bild.png' },
     { name: 'Genesect', image: '/PokemonBilder/Genesect_Bild.png' },
-    { name: 'Infamomo', image: '/PokemonBilder/Infamomo_Bild.png' },
+    { name: 'Volcanion', image: '/PokemonBilder/Volcanion_Bild.png' },
     { name: 'Marshadow', image: '/PokemonBilder/Marshadow_Bild.png' },
-    { name: 'Mew', image: '/PokemonBilder/Mew_Bild.png' },
-    { name: 'Volcanion', image: '/PokemonBilder/Volcanion_Bild.png' }
+    { name: 'Melmetal', image: '/PokemonBilder/Melmetal_Bild.png' },
+    { name: 'Zarude', image: '/PokemonBilder/Zarude_Bild.png' },
+    { name: 'Infamomo', image: '/PokemonBilder/Infamomo_Bild.png' }
   ];
 }
